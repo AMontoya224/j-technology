aadf9a1410fc2be722e794557bd6d555fbbd8f7a
AMontoya224 <montyang224@gmail.com>
2022-11-18 17:21:22 -0600

    new submit

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -14,6 +14,7 @@ function AboutUs( props ) {
       <Left selectLeft={selectLeft} onSelectLeft={onSelectLeft} testRef1={testRef1} testRef2={testRef2} testRef3={testRef3} testRef4={testRef4}/>
       <main>
         <h1>{selectLan ? 'About Us' : 'Sobre Nosotros'}</h1>
+        <p>Somos una empresa joven...</p>
       </main>
     </div>
   );
